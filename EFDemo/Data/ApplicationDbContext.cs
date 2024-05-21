fa833a178cb44856df0ef085e4655cc2036e0c10
MandarVaidya17 <mandarvaidya1711@gmail.com>
2024-05-21 14:28:05 +0530

    Add project files.

---
+++ EFDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFDemo.Models;
+using Microsoft.EntityFrameworkCore;
+namespace EFDemo.Data
+{
+    //applicationDbContext-child class ,DBcontex -parent class of EF
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EFDemo/Models/ProductDAL.cs
@@ -0,0 +1,53 @@
+﻿using EFDemo.Data;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace EFDemo.Models
+{
+    public class ProductDAL
+    {
+        private ApplicationDbContext db;
+
+        public ProductDAL(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Product> GetProducts()
+        {
+            var model = (from product in db.Products
+                      select product).ToList();
+
+                             return model;
+            //return db.Products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            var model=db.Products.Where(x=>x.Id==id).SingleOrDefault();
+            return model;
+        }
+
+        public int AddProduct(Product product)
+        {
+            int result = 0;
+            db.Products.Add(product);
+            db.SaveChanges();
+            return result;
+        }
+        public int EditProduct(Product product)
+        {
+            int result = 0;
+            var model = db.Products.Where(x => x.Id == product.Id).SingleOrDefault();
+            if (model != null)
+            {
+                model.Name = product.Name; // model will hold old data
+                model.Price = product.Price;
+                
+                result = db.SaveChanges();
+
+            }
+            return result;
+
+        }
+    }
+}
